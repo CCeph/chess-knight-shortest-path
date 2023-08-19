3c8546540d2aedd342b485f977fef280b4079b34
CCeph <yousefelsisyy@gmail.com>
2023-08-19 00:53:23 -0300

    Create ability to create new gameboard

--- src/index.js
+++ src/index.js
@@ -2,3 +2,41 @@
 import mergeSort from "./commonUtils.js";
 
 console.log(mergeSort);
+
+const gameBoard = {
+  prettyPrint(node, prefix = "", isLeft = true) {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      this.prettyPrint(
+        node.right,
+        `${prefix}${isLeft ? "│   " : "    "}`,
+        false
+      );
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  },
+
+  createKnightNode(position) {
+    const possibleMoves = [
+      { x: position.x + 1, y: position.y + 2 },
+      { x: position.x + 2, y: position.y + 1 },
+      { x: position.x + 2, y: position.y - 1 },
+      { x: position.x + 1, y: position.y - 2 },
+      { x: position.x - 1, y: position.y - 2 },
+      { x: position.x - 2, y: position.y - 1 },
+      { x: position.x - 2, y: position.y + 1 },
+      { x: position.x - 1, y: position.y + 2 },
+    ];
+
+    return { position, possibleMoves };
+  },
+
+  createGameBoardTree(knightPosition = undefined) {
+    return { root: this.createKnightNode(knightPosition) };
+  },
+};
